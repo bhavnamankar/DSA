c306ca95087a6aa9e9551a811dc5e4cef3330481
Bhavna Mankar <93175501+bhavnamankar@users.noreply.github.com>
2023-06-07 11:40:59 +0530

    Add files via upload
    flowcontrol

---
+++ Flowcontrol.java
@@ -0,0 +1,15 @@
+package basic.company;
+
+public class Flowcontrol {
+    public static void main(String[] args) {
+       int a=150 ,b=15;
+
+       if(a>b){
+           System.out.println(a);
+       }
+       else{
+           System.out.println(b);
+       }
+
+    }
+}
